ce630b565b70dabbe8b598e9610526a8c8ffa73e
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2025-02-14 12:05:01 +0530

    Add admin login page with styling and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FirstPage from './pages/firstPage';
 import UserType from './pages/userType';
 import ServiceProviderRegister from './pages/serviceProvider/register';
+import AdminLogin from './pages/admin/login';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<FirstPage />} />
         <Route path="/user-type" element={<UserType />} />
         <Route path="/service-provider/register" element={<ServiceProviderRegister />} />
+        <Route path="/admin" element={<AdminLogin />} />
       </Routes>
     </Router>
   );
